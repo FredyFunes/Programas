3c6bf0dbebab8e667d09bbc5c23eec0fb0a13aa6
FredyFunes <smss034922@ugb.edu.sv>
2022-08-11 10:03:23 -0600

    Version 1

---
+++ Actividad1 PC/Actividad1 PC/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace act1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entero;
+            double hora = 60;
+            double seg = 60 * 60;
+            int horatotal = 
+            Console.Write("************************Ejercicio1************************************");
+            Console.Write("Ingrese hora que desea convertir: ");
+            entero = int.Parse(Console.ReadLine());
+            Console.Write("La conversion es de: " + hora*60);
+            Console.Write("La conversion de segundos es de: " + hora*60*60);
+            Console.Read();
+
+        }
+    }
+}
+
