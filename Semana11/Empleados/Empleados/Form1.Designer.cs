b3b118289a3c15c4fd2618686e750f60f42ab44e
FredyFunes <smss034922@ugb.edu.sv>
2022-10-08 11:07:29 -0600

    Actividades

---
+++ Semana11/Empleados/Empleados/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿
+namespace Empleados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelDUI = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDUI = new System.Windows.Forms.TextBox();
+            this.labelRegistro = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.txtAFP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Vertodo = new System.Windows.Forms.Button();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DUI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Salario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AFP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(850, 37);
+            this.labelID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(0, 17);
+            this.labelID.TabIndex = 0;
+            this.labelID.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.BackColor = System.Drawing.Color.MidnightBlue;
+            this.labelNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNombre.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelNombre.Location = new System.Drawing.Point(146, 105);
+            this.labelNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(74, 20);
+            this.labelNombre.TabIndex = 1;
+            this.labelNombre.Text = "Nombre";
+            // 
+            // labelDUI
+            // 
+            this.labelDUI.AutoSize = true;
+            this.labelDUI.BackColor = System.Drawing.Color.MidnightBlue;
+            this.labelDUI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDUI.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.labelDUI.Location = new System.Drawing.Point(146, 156);
+            this.labelDUI.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDUI.Name = "labelDUI";
+            this.labelDUI.Size = new System.Drawing.Size(41, 20);
+            this.labelDUI.TabIndex = 2;
+            this.labelDUI.Text = "DUI";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.White;
+            this.txtNombre.Location = new System.Drawing.Point(313, 105);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(132, 22);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtDUI
+            // 
+            this.txtDUI.BackColor = System.Drawing.Color.White;
+            this.txtDUI.Location = new System.Drawing.Point(313, 156);
+            this.txtDUI.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDUI.Name = "txtDUI";
+            this.txtDUI.Size = new System.Drawing.Size(132, 22);
+            this.txtDUI.TabIndex = 5;
+            // 
+            // labelRegistro
+            // 
+            this.labelRegistro.AutoSize = true;
+            this.labelRegistro.BackColor = System.Drawing.Color.Maroon;
+            this.labelRegistro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRegistro.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelRegistro.Location = new System.Drawing.Point(146, 352);
+            this.labelRegistro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelRegistro.Name = "labelRegistro";
+            this.labelRegistro.Size = new System.Drawing.Size(0, 20);
+            this.labelRegistro.TabIndex = 7;
+            this.labelRegistro.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // txtAFP
+            // 
+            this.txtAFP.Location = new System.Drawing.Point(313, 239);
+            this.txtAFP.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAFP.Name = "txtAFP";
+            this.txtAFP.Size = new System.Drawing.Size(132, 22);
+            this.txtAFP.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(146, 196);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Salario";
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(313, 196);
+            this.txtSalario.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(132, 22);
+            this.txtSalario.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(146, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "AFP";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(140, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 32);
+            this.button1.TabIndex = 13;
+            this.button1.Text = " Registro";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.Location = new System.Drawing.Point(890, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 22);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(770, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 36);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Consultar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Nombre,
+            this.DUI,
+            this.Salario,
+            this.AFP});
+            this.dataGridView1.Location = new System.Drawing.Point(770, 94);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(672, 150);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // Vertodo
+            // 
+            this.Vertodo.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Vertodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Vertodo.Location = new System.Drawing.Point(1110, 29);
+            this.Vertodo.Name = "Vertodo";
+            this.Vertodo.Size = new System.Drawing.Size(124, 31);
+            this.Vertodo.TabIndex = 20;
+            this.Vertodo.Text = "Ver todo";
+            this.Vertodo.UseVisualStyleBackColor = false;
+            this.Vertodo.Click += new System.EventHandler(this.Vertodo_Click);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            this.Id.Width = 125;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 125;
+            // 
+            // DUI
+            // 
+            this.DUI.HeaderText = "DUI";
+            this.DUI.MinimumWidth = 6;
+            this.DUI.Name = "DUI";
+            this.DUI.Width = 125;
+            // 
+            // Salario
+            // 
+            this.Salario.HeaderText = "Salario";
+            this.Salario.MinimumWidth = 6;
+            this.Salario.Name = "Salario";
+            this.Salario.Width = 125;
+            // 
+            // AFP
+            // 
+            this.AFP.HeaderText = "AFP";
+            this.AFP.MinimumWidth = 6;
+            this.AFP.Name = "AFP";
+            this.AFP.Width = 125;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1127, 282);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 35);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Modificar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btEliminar
+            // 
+            this.btEliminar.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEliminar.Location = new System.Drawing.Point(770, 282);
+            this.btEliminar.Name = "btEliminar";
+            this.btEliminar.Size = new System.Drawing.Size(78, 39);
+            this.btEliminar.TabIndex = 22;
+            this.btEliminar.Text = "Eliminar";
+            this.btEliminar.UseVisualStyleBackColor = false;
+            this.btEliminar.Click += new System.EventHandler(this.btEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1553, 410);
+            this.Controls.Add(this.btEliminar);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Vertodo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAFP);
+            this.Controls.Add(this.labelRegistro);
+            this.Controls.Add(this.txtDUI);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.labelDUI);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.labelID);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Registro de empleados";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelDUI;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDUI;
+        private System.Windows.Forms.Label labelRegistro;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAFP;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Vertodo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btEliminar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DUI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Salario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AFP;
+    }
+}
+
---
+++ Semana11/Empleados/Empleados/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Empleados
+{
+    public partial class Form1 : Form
+    {
+
+        Empleados Empleado = new Empleados();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btGuardar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btAFP_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (txtNombre.Text == "")
+            {
+                errorProvider1.SetError(txtNombre, "No ingresó el nombre");
+                txtNombre.Focus();
+                return;
+            }
+
+            errorProvider1.SetError(txtNombre, "");
+
+            if (txtDUI.Text == "")
+            {
+                errorProvider1.SetError(txtDUI, "No ingresó el DUI");
+                txtNombre.Focus();
+                return;
+            }
+
+            errorProvider1.SetError(txtNombre, "");
+
+
+            double Salario;
+
+            if (!double.TryParse(txtSalario.Text, out Salario))
+
+            {
+                errorProvider1.SetError(txtSalario, "No ingresó el salario de forma correcta");
+                txtSalario.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtSalario, "");
+
+
+
+            Empleado.Nombre = txtNombre.Text;
+            Empleado.Dui = txtDUI.Text;
+            Empleado.Salario = Convert.ToDouble(txtSalario.Text);
+            Empleado.Afp = Empleado.AFP(Empleado.Salario);
+            txtAFP.Text = Empleado.AFP(Empleado.Salario).ToString();
+            labelRegistro.Text = "¡Registro guardado!";
+            SqlConnection conexion = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Planilla.mdf;Integrated Security=True;Connect Timeout=30");
+                conexion.Open();
+                
+                string cadena = "insert into [Empleados] (Nombre, DUI, Salario, AFP) values ('" + Empleado.Nombre + "','" + Empleado.Dui + "','" + Empleado.Salario + "','" + Empleado.Afp + "')";
+                SqlCommand comando = new SqlCommand(cadena, conexion);
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Los datos se guardaron correctamente");
+
+                conexion.Close();
+
+            
+
+        }
+
+        private void txtID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Planilla.mdf;Integrated Security=True;Connect Timeout=30");
+            conexion.Open();
+
+            Int32 cod;
+
+            if (!Int32.TryParse(textBox1.Text, out cod))
+
+            {
+                errorProvider1.SetError(textBox1, "No ingresó el salario de forma correcta");
+                txtSalario.Focus();
+                return;
+            }
+            errorProvider1.SetError(textBox1, "");
+
+
+            string cadena = "select id, nombre, dui, salario, afp from Empleados where Id=" + cod;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            SqlDataReader registro = comando.ExecuteReader();
+            if (registro.Read())
+            {
+
+                dataGridView1.Rows[0].Cells[0].Value = registro.GetInt32(0);
+                dataGridView1.Rows[0].Cells[1].Value = registro.GetString(1);
+                dataGridView1.Rows[0].Cells[2].Value = registro.GetString(2);
+                dataGridView1.Rows[0].Cells[3].Value = registro.GetDecimal(3);
+                dataGridView1.Rows[0].Cells[4].Value = registro.GetDecimal(4);
+            }
+            else
+                MessageBox.Show("No existe un Empleado con el código ingresado");
+            conexion.Close();
+
+
+        }
+
+        private void lbDUI_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Planilla.mdf;Integrated Security=True;Connect Timeout=30");
+            conexion.Open();
+            
+            try
+            {
+                string cod = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+
+                string Dnombre = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                string Ddui = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                string Dsalario = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                string DAFP = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+
+                string cadena = "update [Empleados] set nombre='" + Dnombre + "', dui='" + Ddui + "', salario='" + Dsalario + "', afp='" + DAFP + "' where Id=" + cod;
+                SqlCommand comando = new SqlCommand(cadena, conexion);
+                int cant;
+                cant = comando.ExecuteNonQuery();
+                if (cant == 1)
+                {
+                    this.dataGridView1.DataSource = null;
+                    this.dataGridView1.Rows.Clear();
+                    MessageBox.Show("Se modificaron los datos del empleado");
+                }
+            }
+            catch (Exception) { MessageBox.Show("Debe seleccionar un registro"); }
+            conexion.Close();
+
+
+
+        }
+
+        private void Vertodo_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Planilla.mdf;Integrated Security=True;Connect Timeout=30");
+            conexion.Open();
+
+            string cadena = "select id, nombre, dui, salario, afp from Empleados";
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            SqlDataReader registro = comando.ExecuteReader();
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.Rows.Clear();
+            if (registro.HasRows)
+            {
+                while (registro.Read())
+                {
+                    int n = dataGridView1.Rows.Add();
+
+                    dataGridView1.Rows[n].Cells[0].Value = registro.GetInt32(0);
+                    dataGridView1.Rows[n].Cells[1].Value = registro.GetString(1);
+                    dataGridView1.Rows[n].Cells[2].Value = registro.GetString(2);
+                    dataGridView1.Rows[n].Cells[3].Value = registro.GetDecimal(3);
+                    dataGridView1.Rows[n].Cells[4].Value = registro.GetDecimal(4);
+                }
+            }
+            else
+                MessageBox.Show("No existen un registros");
+            conexion.Close();
+
+        }
+
+        private void btEliminar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Planilla.mdf;Integrated Security=True;Connect Timeout=30");
+            conexion.Open();
+
+            string cod = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+
+
+            string cadena = "delete from [Empleados] where Id=" + cod;
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            int cant;
+            cant = comando.ExecuteNonQuery();
+            if (cant == 1)
+            {
+                this.dataGridView1.DataSource = null;
+                this.dataGridView1.Rows.Clear();
+                MessageBox.Show("Se borró el registro");
+            }
+
+            else
+            {
+
+                MessageBox.Show("No seleccionó un registro");
+            }
+
+
+            conexion.Close();
+
+        }
+    }
+
+}
